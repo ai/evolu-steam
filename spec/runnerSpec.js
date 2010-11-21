8e734d11bee5ff3c0bc3258b9162098b97dffeec
Andrey A.I. Sitnik <andrey@sitnik.ru>
2010-11-22 00:39:50 +0300

    Add Runner#_onMessage to receive messages from workers

--- spec/runnerSpec.js
+++ spec/runnerSpec.js
@@ -2,6 +2,8 @@ describe('evoplus.steam.Runner', function() {
     it('should create and init workers', function() {
         var run = new evoplus.steam.Runner('/__spec__/log.js', { a: 1 })
         
+        expect(run.driver).toEqual('/__spec__/log.js')
+        expect(run.options).toEqual({ count: 2, a: 1 })
         expect(run.workers.length).toEqual(2)
         
         var first = null
@@ -28,5 +30,19 @@ describe('evoplus.steam.Runner', function() {
         var run = new evoplus.steam.Runner('/__spec__/log.js', { count: 3 })
         expect(run.workers.length).toEqual(3)
         expect(run.options.count).toEqual(3)
+    })
+    
+    it('should receive messages from workers', function() {
+        var run = new evoplus.steam.Runner('/__spec__/log.js')
+        
+        var received = []
+        run._onMessage = function(name, mes) { received.push([name, mes]) }
+        
+        run.workers[1].postMessage({ command: 'showLog' })
+        waitsFor(function() { return received.length > 0 })
+        runs(function() {
+            var init = { command: 'init', name: 1, options: { count: 2 } }
+            expect(received).toEqual([ [1, [init]] ])
+        })
     })
 })
